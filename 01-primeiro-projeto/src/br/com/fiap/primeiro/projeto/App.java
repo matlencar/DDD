99f7e4d8f92ae26ce71e1bc6fa6d945ffe321485
matlencar <rm94642@fiap.com.br>
2022-03-17 21:04:33 -0300

    Atividade first commit

---
+++ 01-primeiro-projeto/src/br/com/fiap/primeiro/projeto/App.java
@@ -0,0 +1,11 @@
+package br.com.fiap.primeiro.projeto;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola Mundo !");
+
+	}
+
+}
---
+++ 01-primeiro-projeto/src/br/com/fiap/primeiro/projeto/Pessoa.java
@@ -0,0 +1,9 @@
+package br.com.fiap.primeiro.projeto;
+
+public class Pessoa {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
